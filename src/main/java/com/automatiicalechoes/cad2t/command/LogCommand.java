676f4928020440d846b49a8ae1d35675ae533c8e
1121665965 <1121665965@qq.com>
2024-02-20 16:14:52 +0800

    0.3.0

--- src/main/java/com/automatiicalechoes/cad2t/command/LogCommand.java
+++ src/main/java/com/automatiicalechoes/cad2t/command/LogCommand.java
@@ -18,6 +18,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.Set;
+
 @Mod.EventBusSubscriber
 public class LogCommand {
     public static final LiteralArgumentBuilder<CommandSourceStack> CAD2T =
@@ -61,6 +62,12 @@ public class LogCommand {
             sourceStack.sendFailure(Component.translatable("additions no load"));
             return 0;
         }
+
+        if(ChunkAdditionTypes.resourceLocations().isEmpty()){
+            sourceStack.sendFailure(Component.literal("no additions registered"));
+            return 1;
+        }
+
         for (ResourceLocation resourceLocation : ChunkAdditionTypes.resourceLocations()) {
             sourceStack.sendFailure(Component.translatable(resourceLocation.toShortLanguageKey()));
         }
@@ -75,9 +82,15 @@ public class LogCommand {
         Vec3 position = sourceStack.getPosition();
         LevelChunk chunk = sourceStack.getLevel().getChunk((int) position.x >> 4, (int) position.z >> 4);
         Set<ChunkAddition<?>> activeAddition = ((IChunkAccess) chunk).getActiveAddition((int) (position.y - sourceStack.getLevel().getMinBuildHeight()) >> 4);
+        if(activeAddition.isEmpty()){
+            sourceStack.sendFailure(Component.literal("no active additions in chunk section"));
+            return 1;
+        }
+
         for (ChunkAddition<?> chunkAddition : activeAddition) {
             sourceStack.sendFailure(Component.literal(chunkAddition.registerName().toShortLanguageKey()));
         }
+
         return 1;
     }
 
